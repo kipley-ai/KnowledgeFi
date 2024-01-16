76eeadfd13038af101dd5141596c17f9343d085d
Akira Rafhael Janson Pattirane <akira.rafhael@ti.ukdw.ac.id>
2024-01-16 20:55:57 +0800

    Data source page
    Add data source page
    Add upload file page

--- app/(default)/data-source/local.tsx
+++ app/(default)/data-source/local.tsx
@@ -1,5 +1,90 @@
+import CheckIcon from 'public/images/check-icon.svg';
+import CrossIcon from 'public/images/cross-icon.svg';
+import UploadIcon from 'public/images/upload-icon.svg';
+import Image from 'next/image';
+import React, { useCallback, useState } from 'react';
+
 export default function Local() {
+    const [dragActive, setDragActive] = useState(false);
+    const dummyData = [
+        { fileName: "file1.txt", fileSize: "10 KB" },
+        { fileName: "file2.txt", fileSize: "5 KB" },
+        { fileName: "file3.txt", fileSize: "8 KB" }
+    ];
+
+    const handleDrag = useCallback((event:any) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (event.type === 'dragenter' || event.type === 'dragover') {
+          setDragActive(true);
+        } else if (event.type === 'dragleave') {
+          setDragActive(false);
+        }
+      }, []);
+    
+      const handleDrop = useCallback((event:any) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setDragActive(false);
+        const files = event.dataTransfer.files;
+        console.log(files);
+      }, []);
+    
+      const handleChange = useCallback((event:any) => {
+        event.preventDefault();
+        const files = event.target.files;
+        console.log(files);
+      }, []);
+
     return (
-        <>local</>
+        <div className="mx-64">
+            <div
+                className="flex flex-col mt-5 mb-8 border border-dashed border-[#aaa] rounded-xl py-20 px-32 text-center items-center cursor-pointer color-[#aaa] font-inter text-white "
+                onDragEnter={handleDrag}
+                onDragOver={handleDrag}
+                onDragLeave={handleDrag}
+                onDrop={handleDrop}
+                onClick={() => {
+                    const fileInput = document.getElementById('file-input');
+                    if (fileInput) {
+                        fileInput.click();
+                    }
+                }}
+            >
+                <input
+                    type="file"
+                    id="file-input"
+                    multiple
+                    onChange={handleChange}
+                    style={{ display: 'none' }}
+                />
+                <div className="bg-white rounded-full p-6 mb-8">
+                    <Image width={60} height={60} src={UploadIcon} alt="Upload Icon" />
+                </div>
+                <label className="text-md mb-3 font-semibold cursor-pointer" htmlFor="file-input">
+                    Drop your files here OR <span className="text-[#01F7FF]">Click here to browse</span>
+                </label>
+                <p className="text-slate-400 text-xs">Supported file formats: .pdf, .csv, .txt, .json, .pptx, .xlsx, .docx.</p>
+                <p className="text-slate-400 text-xs">Maximum number of files allowed: 10</p>
+            </div>
+            <div>
+                {
+                    dummyData.map((data) => {
+                        return (
+                            <div className="flex py-5 px-8 my-5 rounded-3xl text-white bg-neutral-900 justify-between">
+                                <div className="flex flex-row">
+                                    <Image src={CheckIcon} alt="Check Icon" />
+                                    <div className="flex flex-col ml-8">
+                                        <h3 className="font-semibold">{data.fileName}</h3>
+                                        <p className="text-xs">{data.fileSize}</p>
+                                    </div>
+                                </div>
+                                <Image src={CrossIcon} alt="Cross Icon" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
--- app/(default)/data-source/step-1.tsx
+++ app/(default)/data-source/step-1.tsx
@@ -5,18 +5,19 @@ import FolderAddIcon from "public/images/folder-add.svg";
 import Image from "next/image";
 
 export default function Step1({ selectedButton, setSelectedButton }: { selectedButton: string, setSelectedButton: Function }) {
+    console.log(selectedButton)
     return (
         <div className="mx-64 grid grid-cols-2 gap-4 text-white font-bold mt-10">
-            <button className={`flex flex-col px-20 py-10 items-center border-2 border-[#50575F] ${selectedButton == 'twitter' ? 'border-[#01F7FF]' : ''} rounded-3xl`} onClick={() => setSelectedButton('twitter')}>
-                <Image src={XIcon} alt="X Icon" />
+            <button className={`flex flex-col px-20 py-10 items-center border-2 ${selectedButton == 'twitter' ? 'border-[#01F7FF]' : 'border-[#50575F]'} rounded-3xl`} onClick={() => setSelectedButton('twitter')}>
+                <Image width={48} height={48} src={XIcon} alt="X Icon" />
                 <h3 className="pt-6">Connect Twitter</h3>
             </button>
-            <button className={`flex flex-col px-20 py-10 items-center border-2 border-[#50575F] ${selectedButton == 'notion' ? 'border-[#01F7FF]' : ''} rounded-3xl`} onClick={() => setSelectedButton('notion')}>
-                <Image src={NotionIcon} alt="Notion Icon" />
+            <button className={`flex flex-col px-20 py-10 items-center border-2 ${selectedButton == 'notion' ? 'border-[#01F7FF]' : 'border-[#50575F]'} rounded-3xl`} onClick={() => setSelectedButton('notion')}>
+                <Image width={48} height={48} src={NotionIcon} alt="Notion Icon" />
                 <h3 className="pt-6">Connect Notion</h3>
             </button>
-            <button className={`flex flex-col px-20 py-10 items-center border-2 border-[#50575F] ${selectedButton == 'local' ? 'border-[#01F7FF]' : ''} rounded-3xl`} onClick={() => setSelectedButton('local')}>
-                <Image src={FolderAddIcon} alt="Folder Add Icon" />
+            <button className={`flex flex-col px-20 py-10 items-center border-2 ${selectedButton == 'local' ? 'border-[#01F7FF]' : 'border-[#50575F]'} rounded-3xl`} onClick={() => setSelectedButton('local')}>
+                <Image width={48} height={48} src={FolderAddIcon} alt="Folder Add Icon" />
                 <h3 className="pt-6">Upload files</h3>
             </button>
         </div>
