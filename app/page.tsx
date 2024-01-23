4f1e7c5f558d0a6fbc4abbec415040bf93a05933
let-me-cook <itsalfath123@gmail.com>
2024-01-23 18:43:03 +0800

    feat: added twitter username on `useSession`

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-"use client"
-import { redirect, useSearchParams } from 'next/navigation'
-import { useAccount } from 'wagmi';
+"use client";
+import { redirect, useSearchParams } from "next/navigation";
+import { useAccount } from "wagmi";
 
 export default function Home() {
-  const { isConnected } = useAccount();
-  const searchParams = useSearchParams();
+	const { isConnected } = useAccount();
+	const searchParams = useSearchParams();
 
-  const nextUrl = searchParams.get("next") || "/dashboard";
-  if (isConnected) {
-    console.log(nextUrl)
-    redirect(nextUrl);
-  } else {
-  return <>
-  </>
-  }
+	const nextUrl = searchParams.get("next") || "/dashboard";
+	console.log(isConnected)
+
+	// Alfath: `isConnected` will always return false for me even with metamask ready
+	if (isConnected) {
+		console.log(nextUrl);
+		redirect(nextUrl);
+	} else {
+		return <></>;
+	}
 }
