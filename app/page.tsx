bbe36b36c87d0f5429591006493f2ce9f5890f80
let-me-cook <itsalfath123@gmail.com>
2024-01-25 20:46:27 +0800

    fix: hydration errors from useAccount

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { redirect, useSearchParams } from "next/navigation";
 import { useAccount } from "wagmi";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 	const { isConnected } = useAccount();
+	useEffect(() => {
+		setIsConnected_(isConnected);
+	}, [isConnected]);
+
+	// Hydrate safe
+	const [isConnected_, setIsConnected_] = useState<boolean>(false);
+
 	const searchParams = useSearchParams();
 
 	const nextUrl = searchParams.get("next") || "/dashboard";
-	console.log(isConnected)
+	console.log(isConnected);
 
 	// Alfath: `isConnected` will always return false for me even with metamask ready
-	if (isConnected) {
-		console.log(nextUrl);
+	// if (isConnected_) {
+	// 	console.log(nextUrl);
 		redirect(nextUrl);
-	} else {
-		return <></>;
-	}
+	// } else {
+	// 	return <></>;
+	// }
 }
