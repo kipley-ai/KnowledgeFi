08115cc5a98bf00d066293e518f87b2ada63dd16
ridho-lexikat <ridho@lexikat.com>
2024-02-20 13:23:10 +0700

    manage account improvements

--- hooks/api/user/index.tsx
+++ hooks/api/user/index.tsx
@@ -5,94 +5,134 @@ import axios from "axios";
 import { IPaginate } from "../interfaces";
 
 export const useCreateUser = () => {
-	// const { address } = useAccount();
+  // const { address } = useAccount();
 
-	return useMutation({
-		mutationFn: (address: string) =>
-			axios.post("/api/user/create", { wallet_addr: address }, {
-				headers: {
-					"x-kf-user-id": address,
-				},
-			}),
-	});
+  return useMutation({
+    mutationFn: (address: string) =>
+      axios.post(
+        "/api/user/create",
+        { wallet_addr: address },
+        {
+          headers: {
+            "x-kf-user-id": address,
+          },
+        },
+      ),
+  });
 };
 
 export const useDepositHistory = (
-	params: IPaginate,
-	placeholderData: typeof keepPreviousData | undefined = undefined,
+  params: IPaginate,
+  placeholderData: typeof keepPreviousData | undefined = undefined,
 ) => {
-	const { address } = useAccount();
+  const { address } = useAccount();
 
-	return useQuery({
-		queryKey: ["deposit", params.page],
-		queryFn: () =>
-			axios.post("/api/user/deposit", params, {
-				headers: {
-					"x-kf-user-id": address,
-				},
-			}),
-	});
+  return useQuery({
+    queryKey: ["deposit", params.page],
+    queryFn: () =>
+      axios.post("/api/user/deposit", params, {
+        headers: {
+          "x-kf-user-id": address,
+        },
+      }),
+  });
 };
 
 export const useWithdrawHistory = (
-	params: IPaginate,
-	placeholderData: typeof keepPreviousData | undefined = undefined,
+  params: IPaginate,
+  placeholderData: typeof keepPreviousData | undefined = undefined,
 ) => {
-	const { address } = useAccount();
+  const { address } = useAccount();
 
-	return useQuery({
-		queryKey: ["withdraw", params.page],
-		queryFn: () =>
-			axios.post("/api/user/withdraw", params, {
-				headers: {
-					"x-kf-user-id": address,
-				},
-			}),
-	});
+  return useQuery({
+    queryKey: ["withdraw", params.page],
+    queryFn: () =>
+      axios.post("/api/user/withdraw", params, {
+        headers: {
+          "x-kf-user-id": address,
+        },
+      }),
+  });
 };
 export const useUpdateUserAPI = () => {
-	const { address } = useAccount();
+  const { address } = useAccount();
 
-	return useMutation({
-		mutationFn: (params: IUpdateUserParams) =>
-			axios.post("/api/user/update", params, {
-				headers: {
-					"x-kf-user-id": address,
-				},
-			}),
-	});
+  return useMutation({
+    mutationFn: (params: IUpdateUserParams) =>
+      axios.post("/api/user/update", params, {
+        headers: {
+          "x-kf-user-id": address,
+        },
+      }),
+  });
 };
 
 export const useEarningReport = (
-	params: IPaginate,
-	placeholderData: typeof keepPreviousData | undefined = undefined,
+  params: IPaginate,
+  placeholderData: typeof keepPreviousData | undefined = undefined,
 ) => {
-	const { address } = useAccount();
-	
-	return useQuery({
-		queryKey: ["earning", params.page],
-		queryFn: () =>
-			axios.post("/api/user/earning", params, {
-				headers: {
-					"x-kf-user-id": address,
-				},
-			}),
-	});
+  const { address } = useAccount();
+
+  return useQuery({
+    queryKey: ["earning", params.page],
+    queryFn: () =>
+      axios.post("/api/user/earning", params, {
+        headers: {
+          "x-kf-user-id": address,
+        },
+      }),
+  });
 };
 
 export const useCreditUsage = (
-	params: IPaginate,
-	placeholderData: typeof keepPreviousData | undefined = undefined,
+  params: IPaginate,
+  placeholderData: typeof keepPreviousData | undefined = undefined,
 ) => {
-	const { address } = useAccount();
-	
-	return useQuery({
-		queryKey: ["credit", params.page],
-		queryFn: () =>
-			axios.post("/api/user/credit", params, {
-				headers: {
-					"x-kf-user-id": address,
-				},
-			}),
-	});
+  const { address } = useAccount();
+
+  return useQuery({
+    queryKey: ["credit", params.page],
+    queryFn: () =>
+      axios.post("/api/user/credit", params, {
+        headers: {
+          "x-kf-user-id": address,
+        },
+      }),
+  });
+};
+
+export const useUserDetail = () => {
+  const { address } = useAccount();
+
+  return useQuery({
+    queryKey: ["user-detail"],
+    queryFn: () =>
+      axios.post(
+        "/api/user/detail",
+        {},
+        {
+          headers: {
+            "x-kf-user-id": address,
+          },
+        },
+      ),
+  });
+};
+
+export const useCreatorOverview = () => {
+  const { address } = useAccount();
+
+  return useQuery({
+    queryKey: ["creator-overview"],
+    queryFn: () =>
+      axios.post(
+        "/api/user/creator_overview",
+        {},
+        {
+          headers: {
+            "x-kf-user-id": address,
+          },
+        },
+      ),
+  });
 };
